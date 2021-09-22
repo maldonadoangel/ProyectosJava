5ebfda8c65437f9fa3f1bbfec2761de9d516a765
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-22 03:31:00 -0500

    Sistema Numerico con validación de entradas

--- SistemaNumerico/src/SistemaNumerico.java
+++ SistemaNumerico/src/SistemaNumerico.java
@@ -2,9 +2,17 @@ import javax.swing.*;
 
 public class SistemaNumerico {
     public static void main(String[] args){
+        int numeroDecimal = 0;
         System.out.println("Sistema Numerico");
         String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero entero: ");
-        int numeroDecimal = Integer.parseInt(numeroStr);
+        try{
+             numeroDecimal = Integer.parseInt(numeroStr);
+        }catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error, ingrese un numero valido");
+            main(args);
+            System.exit(0);
+        }
+
         System.out.println("El valor binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
         System.out.println("El valor octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
         System.out.println("El valor Hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));

1f1cdb817bdaf9f169cc15f1fc40b470e8c59fdd
Angel Morales <maldonadoangel7069@gmail.com>
2021-09-22 03:22:54 -0500

    Sistema Numericos

--- SistemaNumerico/src/SistemaNumerico.java
+++ SistemaNumerico/src/SistemaNumerico.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class SistemaNumerico {
     public static void main(String[] args){
-        System.out.println("Iniciando proyecto");
+        System.out.println("Sistema Numerico");
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero entero: ");
+        int numeroDecimal = Integer.parseInt(numeroStr);
+        System.out.println("El valor binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        System.out.println("El valor octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+        System.out.println("El valor Hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        String mensaje = "El valor binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        mensaje += "\nEl valor octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        mensaje += "\nEl valor Hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 }
